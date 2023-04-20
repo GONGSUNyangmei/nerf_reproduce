9848c62c41cf45c31f04bdf7a3886a4e9e97dbb9
GONGSUNyangmei <2484803754@qq.com>
2023-04-20 12:06:30 +0800

    fix one bug

--- position_encoding.py
+++ position_encoding.py
@@ -10,12 +10,14 @@ def get_embedder(multires, i=0):
 
     out_dim = 3
     embed_fns = []
+    #include x
     embed_fns.append(lambda x : x)
     freq_bands = 2.**torch.linspace(0., multires-1, steps=multires)
     
+    ## each sin/cos add 3 dim
     for freq in freq_bands:
         embed_fns.append(lambda x : torch.sin(x * freq))
         embed_fns.append(lambda x : torch.cos(x * freq))
-        out_dim += 3
+        out_dim += 6
     embed = lambda x:torch.cat([fn(x) for fn in embed_fns], -1)
     return embed, out_dim
